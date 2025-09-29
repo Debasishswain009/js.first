0cb4d17e9f0266fad00eeb7833400a68f0c2d827
Debasish swain <debasishswain0087@gmail.com>
2025-09-29 15:35:29 +0000

    data type and variable done

---
+++ .devcontainer/cwc_02_variables.js
@@ -0,0 +1,10 @@
+const accountId = 134
+let accountEmail = "debasisishswain0087@gmail.com"
+var accountPassword ="1234"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2// not allowed
+
+accountEmail = "hc@kc.com"
+accountCity = "paradeep"
+console.table([accountEmail,accountId,accountPassword,accountState])
---
+++ .devcontainer/cwc_03.js
@@ -0,0 +1,14 @@
+"use strict";//uses newer version of javascript for all code
+//alert("hii");//here we are using nodejs not browser
+let name = "kanha";
+let age = 12;
+age = 18;
+let lie = true;
+let how = null;
+let what;
+console.log(age);
+console.log(typeof age);
+console.log(typeof lie);
+console.log(typeof name);
+console.log(typeof how);//object
+console.log(typeof what);//undefined
