0e4b7fa399f571fc5c788bd85607654550c7358c
Debasish swain <debasishswain0087@gmail.com>
2025-09-29 23:17:00 -0700

    cwc_04.js
    operators done

--- .devcontainer/cwc_04.js
+++ .devcontainer/cwc_04.js
@@ -34,21 +34,62 @@
 //1<Boolean>----------->>true   
 //0<boolean>---------->>false
 
-let name = "Harry";
-let nameToBoolean = Boolean(name);
-console.log(nameToBoolean);
+// let name = "Harry";
+// let nameToBoolean = Boolean(name);
+// console.log(nameToBoolean);
 
-let nameToNum = Number(name);
-console.log(nameToNum);
+// let nameToNum = Number(name);
+// console.log(nameToNum);
 
-let num;
-let numToNum = Number(num);
-console.log(numToNum);
+// let num;
+// let numToNum = Number(num);
+// console.log(numToNum);
 
-let nam = null;
-let namToNum = Number(nam);
-console.log(namToNum);
+// let nam = null;
+// let namToNum = Number(nam);
+// console.log(namToNum);
 
 // null<number> -------->> 0
 // undefined<number> --->> NaN
 //string<number> ------->> NaN
+
+//----------operation--------------
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1 = 'hello';
+// let str2 = ' kanha';
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// some tricky parts
+  
+// console.log(2 + "2");
+// console.log("2" + 2);
+// console.log(2 + 1 + "2");
+// console.log(2 + "2" + 1 );
+// console.log("2" + 1 + 2);//precidence matters
+
+// console.log(+true);
+// console.log(true+);//eror
+// console.log(+"");
+
+// USE OF INCREMENT AND DECREAMENT
+//  let num1,num2,num3;
+//  num1 = num2 = num3 = 2 + 2;//not a good practice
+//  console.table([num1,num2,num3]);
+
+
+let gamecounter = 100;
+console.log(gamecounter);
+let x = gamecounter++;
+console.table([gamecounter,x])
+
+let game = 100;
+console.log(game);
+let y = ++game;
+console.table([gamecounter,y])
